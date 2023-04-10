9fa8a96143cb75d6c88a31301abc714e72dd695d
Monil Sorathia <monilsorathia01@gmail.com>
2023-04-09 17:10:40 -0700

    first commit

---
+++ src/main/java/edu/usc/csci310/project/watchlist/watchlistController.java
@@ -0,0 +1,144 @@
+package edu.usc.csci310.project.watchlist;
+
+import edu.usc.csci310.project.models.User;
+import edu.usc.csci310.project.loginsignup.UserController;
+import edu.usc.csci310.project.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+@RestController
+@RequestMapping("/watchlistController")
+public class watchlistController {
+    //public Hashtable<String, User> userdatabase = new Hashtable<String, User>();
+    @Autowired
+    public UserRepository userRepository;
+
+    public static String encrypt(String plaintext, int shift) {
+        StringBuilder ciphertext = new StringBuilder();
+        for (int i = 0; i < plaintext.length(); i++) {
+            char c = plaintext.charAt(i);
+            if (Character.isLetter(c)) {
+                if (Character.isUpperCase(c)) {
+                    ciphertext.append((char) ('A' + (c - 'A' + shift) % 26));
+                } else {
+                    ciphertext.append((char) ('a' + (c - 'a' + shift) % 26));
+                }
+            } else if (Character.isDigit(c))
+            {
+                ciphertext.append((char) ('0' + (c - '0' + shift) % 10));
+            } else
+            {
+                ciphertext.append(c);
+            }
+        }
+        return ciphertext.toString();
+    }
+
+    public static String decrypt(String ciphertext) {
+        return encrypt(ciphertext, 21);
+    }
+
+    @PostMapping("/createwatchlist")
+    public String createList(@RequestParam String watchlistname, @RequestParam String email) {
+        //encrypting
+        UserController userController = new UserController();
+        userController.signUp("john", "dummywatchlist@usc.edu", "1234", "1234");
+        String en_email = encrypt(email, 5);
+
+        User user = userRepository.findByEmail(en_email);
+        if (user != null) {
+            String en_name = encrypt(watchlistname, 5);
+
+            Hashtable<String, ArrayList<String>> alllists = user.getAllWatchlists();
+            ArrayList<String> specificlist = alllists.get(en_name);
+            if (specificlist == null) {
+                user.createnewList(en_name);
+                String responseString = "{\"success\": \"" + "true" + "\"," +
+                        "\"failure\": \"" + "false" + "\"}";
+                return responseString;
+            } else {
+                String responseString = "{\"success\": \"" + "false" + "\"," +
+                        "\"failure\": \"" + "true" + "\"}";
+                return responseString;
+            }
+        }
+        String responseString = "{\"success\": \"" + "false" + "\"," +
+                "\"failure\": \"" + "true" + "\"}";
+        return responseString;
+    }
+
+    @PostMapping("/retrievelist")
+    public String retrieveList(@RequestParam String email, @RequestParam String watchlistname)
+    {
+        UserController userController = new UserController();
+        userController.signUp("john", "dummywatchlist@usc.edu", "1234", "1234");
+        //userController.userRepository = userRepository;
+        String en_email = encrypt(email, 5);
+        User user = userRepository.findByEmail(en_email);
+        if(user != null)
+        {
+            String en_name = encrypt(watchlistname, 5);
+
+            Hashtable<String, ArrayList<String>> alllists = user.getAllWatchlists();
+            ArrayList<String> specificlist = alllists.get(en_name);
+            if(specificlist == null)
+            {
+                return "";
+            }
+            else
+            {
+                String id = "";
+                String responseString = "[ ";
+                for(int i = 0; i < specificlist.size(); i++)
+                {
+                    String encrypted_id = specificlist.get(i);
+                    responseString = responseString + decrypt(encrypted_id) + ", ";
+                }
+                responseString = responseString + "]";
+                return responseString;
+            }
+
+        }
+        return "";
+    }
+
+
+    @PostMapping("/addtolist")
+    public String addtolist(@RequestParam String email, @RequestParam String watchlistname, @RequestParam String id)
+    {
+        UserController userController = new UserController();
+        userController.signUp("john", "dummywatchlist@usc.edu", "123456789", "123456789");
+        //userController.userRepository = userRepository;
+        String en_email = encrypt(email, 5);
+        System.out.println(email + watchlistname + id);
+
+        User user = userRepository.findByEmail(en_email);
+        if(user != null)
+        {
+            System.out.println("Entered");
+            String en_name = encrypt(watchlistname, 5);
+            Hashtable<String, ArrayList<String>> alllists = user.getAllWatchlists();
+            ArrayList<String> specificlist = alllists.get(en_name);
+            if(specificlist == null)
+            {
+                String responseString = "{\"success\": \"" + "false" + "\"," +
+                        "\"failure\": \"" + "true" + "\"}";
+                System.out.println("Null");
+                return responseString;
+            }
+            else {
+                user.addtowatchlist(id, encrypt(watchlistname, 5));
+                String responseString = "{\"success\": \"" + "true" + "\"," +
+                        "\"failure\": \"" + "false" + "\"}";
+                System.out.println("Not Null");
+                return responseString;
+            }
+        }
+        String responseString = "{\"success\": \"" + "false" + "\"," +
+                "\"failure\": \"" + "true" + "\"}";
+        return responseString;
+    }
+}
---
+++ src/test/java/edu/usc/csci310/project/loginsignup/UserControllerTest.java
@@ -0,0 +1,124 @@
+package edu.usc.csci310.project.loginsignup;
+
+
+import edu.usc.csci310.project.models.User;
+import edu.usc.csci310.project.repositories.UserRepository;
+import edu.usc.csci310.project.loginsignup.UserController;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+public class UserControllerTest {
+
+    @Test
+    public void signUpFirst() {         //passwords do not match
+        UserController user = new UserController();
+        String answer = user.signUp("monil", "monil@usc.edu", "123", "1234");
+
+        String expected = "{\"success\": \"" + "failure" + "\"," +
+                "\"passwordmatch\": \"" + "false" + "\"}";
+        assertEquals(expected, answer);
+    }
+
+    @Test
+    public void signUpSecond() {                //password length incorrect
+        UserController user = new UserController();
+        String answer = user.signUp("monil", "monil@usc.edu", "1234", "1234");
+
+        String expected = "{\"success\": \"" + "failure" + "\"," +
+                "\"passwordmatch\": \"" + "true" + "\"," +
+                "\"passwordLength\": \"" + "false" + "\"}";
+        assertEquals(expected, answer);
+    }
+
+    @Test
+    public void signUpThird() {         //user already exists
+        UserController user = new UserController();
+        UserRepository mockrepo = mock(UserRepository.class);
+        user.userRepository = mockrepo;
+        User newuser = new User("temp", "temp", "temp");
+
+        when(mockrepo.findByEmail(anyString())).thenReturn(newuser);
+
+        String answer = user.signUp("monil", "monil@usc.edu", "12345678", "12345678");
+
+        String expected = "{\"success\": \"" + "failure" + "\"," +
+                "\"passwordmatch\": \"" + "true" + "\"," +
+                "\"userexists\": \"" + "false" + "\"," +
+                "\"passwordLength\": \"" + "true" + "\"}";
+        assertEquals(expected, answer);
+    }
+
+    @Test
+    public void signUpFourth() {         //successful sign up
+        UserController user = new UserController();
+        UserRepository mockrepo = mock(UserRepository.class);
+        user.userRepository = mockrepo;
+        User newuser = new User("temp", "temp", "temp");
+
+        when(mockrepo.findByEmail(anyString())).thenReturn(null);
+        when(mockrepo.save(any())).thenReturn(null);
+        String answer = user.signUp("monil", "monil@usc.edu", "12345678", "12345678");
+
+        String expected = "{\"success\": \"" + "true" + "\"}";
+        assertEquals(expected, answer);
+    }
+    @Test
+    public void loginFirst() {
+        UserController user = new UserController();
+        UserRepository mockrepo = mock(UserRepository.class);
+        user.userRepository = mockrepo;
+
+        String answer = user.login("temp", "temp");
+        when(mockrepo.findByEmail(anyString())).thenReturn(null);
+
+        String expected = "{\"success\": \"" + "failure" + "\"," +
+                "\"loginuserexists\": \"" + "false" + "\"}";
+
+        assertEquals(expected, answer);
+    }
+
+
+    @Test
+    public void loginSecond() {
+        UserController user = new UserController();
+        UserRepository mockrepo = mock(UserRepository.class);
+        user.userRepository = mockrepo;
+
+        User mockuser = mock(User.class);
+        when(mockrepo.findByEmail(anyString())).thenReturn(mockuser);
+
+
+        when(mockuser.getPassword()).thenReturn("");
+        String answer = user.login("temp", "temp");
+        String expected = "{\"success\": \"" + "failure" + "\"," +
+                "\"loginpasswordmatch\":\"" + "false" + "\"," +
+                "\"loginuserexists\": \"" + "true" + "\"}";
+        assertEquals(expected, answer);
+    }
+    @Test
+    public void loginThird() {
+        UserController user = new UserController();
+        UserRepository mockrepo = mock(UserRepository.class);
+        user.userRepository = mockrepo;
+
+        User mockuser = mock(User.class);
+        mockuser.setEmail("temp");
+        mockuser.setPassword("temp");
+        mockuser.setName("temp");
+        when(mockrepo.findByEmail(anyString())).thenReturn(mockuser);
+
+        String en_password = "";
+        String password = "temp";
+        en_password = UserController.encrypt(password, 5);
+
+        when(mockuser.getPassword()).thenReturn(en_password);
+        String expected = "{\"success\": \"" + "true" + "\"}";
+        String answer = user.login("temp", "temp");
+
+        assertEquals(expected, answer);
+    }
+}
